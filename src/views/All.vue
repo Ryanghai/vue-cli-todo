2ee12ef85995f77938502b17f24ebfb7946524b8
GitHub-Ryanghai <68055743+Ryanghai@users.noreply.github.com>
2021-07-06 15:51:38 +0900

    push for test

--- src/views/All.vue
+++ src/views/All.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an All page</h1>
   </div>
+  <!-- <div class="add_task">
+    <input
+      type="text"
+      v-model="body"
+      @keydown.enter="addList"
+      placeholder="New Task"
+    />
+    <input type="submit" value="+" :disabled="body === ''" @click="addList" />
+  </div>
+  <p>残タスク数：{{ finishedRate }}/{{ unfinishedTodoCount }}個</p>
+  <p>終了率：{{ finishedRate }}%</p>
+  <div class="tasks">
+    <ul>
+      <li v-for="(list, i) in lists" v-bind:key="i">
+        <div class="text">
+          <input type="checkbox" v-model="list.done" />
+          <span :class="list.done ? 'finished' : ''">
+            {{ list.body }}
+          </span>
+        </div>
+        <button @click="deleteList(i)">×</button>
+      </li>
+    </ul>
+  </div> -->
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -61,30 +61,6 @@
         </div>
       </div>
     </div>
-    <!-- <div class="add_task">
-      <input
-        type="text"
-        v-model="body"
-        @keydown.enter="addList"
-        placeholder="New Task"
-      />
-      <input type="submit" value="+" :disabled="body === ''" @click="addList" />
-    </div>
-    <p>残タスク数：{{ finishedRate }}/{{ unfinishedTodoCount }}個</p>
-    <p>終了率：{{ finishedRate }}%</p>
-    <div class="tasks">
-      <ul>
-        <li v-for="(list, i) in lists" v-bind:key="i">
-          <div class="text">
-            <input type="checkbox" v-model="list.done" />
-            <span :class="list.done ? 'finished' : ''">
-              {{ list.body }}
-            </span>
-          </div>
-          <button @click="deleteList(i)">×</button>
-        </li>
-      </ul>
-    </div> -->
   </div>
 </template>
 
