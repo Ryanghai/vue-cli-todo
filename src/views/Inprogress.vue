91caca06c0798821ef5a15aa1061f37e0fa165f1
GitHub-Ryanghai <68055743+Ryanghai@users.noreply.github.com>
2021-07-29 14:04:20 +0900

    before prepare todo page

--- src/views/Inprogress.vue
+++ src/views/Inprogress.vue
@@ -10,13 +10,45 @@
           <div class="title-image task-list">
             <img src="@/assets/todo-image.svg" alt="todo-image">
           </div>
-          <p>inprogress</p>
+          <p>Inprogress</p>
+        </div>
+        <div class="task-lists">
+          <div class="task-lists-list" v-for="(todotask, i) in todotasks" :key="i">
+            <label>
+              <input class="checkbox-input" type="checkbox" />
+              <p class="checkbox-parts">{{ todotask.label }}</p>
+            </label>
+            <a href="#">
+              <img class="to-memo" src="@/assets/to-memo.svg" alt="">
+            </a>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </div>
 </template>
+
+<script>
+import { todoTasks } from '../data/todotasks.js'
+
+export default {
+  data: () => ({
+    lists: JSON.parse(window.localStorage.getItem("lists")) || [],
+    body: "",
+    todotasks: todoTasks,
+  }),
+  methods: {
+    addList: function() {
+      if (this.body === "") return;
+      this.lists.push({ body: this.body, done: false });
+      window.localStorage.setItem("lists", JSON.stringify(this.lists));
+      this.body = "";
+    },
+  },
+}
+</script>
+
 
 <style scoped>
 .todo-container {
@@ -67,7 +99,7 @@
 }
 
 .task-list {
-  background-color: #fabe00;
+  background-color: #bf6dea;
 }
 .task-list img {
   width: 30px;
@@ -80,5 +112,83 @@
   font-size: 24px;
   color: #757575;
 }
+
+.task-lists-list {
+  display: flex;
+  margin: 12px 6px 0;
+  padding: 14px 8px;
+  border: 1px solid #B5B5B5;
+  border-radius: 12px;
+}
+
+.task-lists-list:first-child {
+  margin-top: 72px;
+}
+
+.task-lists-list label {
+  width: 100%;
+  display: flex;
+}
+
+.task-lists-list .to-memo {
+  width: 10px;
+  height: 10px;
+  margin: auto 4px;
+}
+.task-lists-list .trash {
+  width: 15px;
+  height: 15px;
+  margin: auto 0px;
+  opacity: 1;
+}
+
+.task-lists-list input[type="checkbox" i] {
+  margin: auto 0;
+  width: 20px;
+  height: 20px;
+  margin-right: 10px;
+}
+
+.task-lists-list p {
+  text-align: left;
+  font-size: 14px;
+  width: 90%;
+  max-width: 260px;
+}
+
+.checkbox-input {
+  display: none;
+}
+.checkbox-parts {
+  padding-left: 24px;
+  position: relative;
+}
+.checkbox-parts::before {
+  content: "";
+  display: block;
+  position: absolute;
+  top: 50%;
+  margin-top: -10px;
+  left: 0;
+  width: 15px;
+  height: 15px;
+  background-color: white;
+  border: 1px solid #bf6dea;
+  border-radius: 50%;
+}
+.checkbox-input:checked + .checkbox-parts::after {
+  content: "";
+  display: block;
+  position: absolute;
+  top: 50%;
+  margin-top: -15px;
+  left: 5px;
+  width: 7px;
+  height: 14px;
+  transform: rotate(40deg);
+  border-bottom: 3px solid #bf6dea;
+  border-right: 3px solid #bf6dea;
+}
+
 
 </style>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -12,6 +12,17 @@
           </div>
           <p>To do</p>
         </div>
+        <div class="task-lists">
+          <div class="task-lists-list" v-for="(todotask, i) in todotasks" :key="i">
+            <label>
+              <input class="checkbox-input" type="checkbox" />
+              <p class="checkbox-parts">{{ todotask.label }}</p>
+            </label>
+            <a href="#">
+              <img class="to-memo" src="@/assets/to-memo.svg" alt="">
+            </a>
+          </div>
+        </div>
         <div class="add-task-container">
           <div class="add-task">
             <input type="submit" value="+" :disabled="body === ''" @click="addList" />
@@ -28,6 +39,27 @@
   </div>
 </div>
 </template>
+
+<script>
+import { todoTasks } from '../data/todotasks.js'
+
+export default {
+  data: () => ({
+    lists: JSON.parse(window.localStorage.getItem("lists")) || [],
+    body: "",
+    todotasks: todoTasks,
+  }),
+  methods: {
+    addList: function() {
+      if (this.body === "") return;
+      this.lists.push({ body: this.body, done: false });
+      window.localStorage.setItem("lists", JSON.stringify(this.lists));
+      this.body = "";
+    },
+  },
+}
+</script>
+
 
 <style scoped>
 .todo-container {
@@ -136,5 +168,83 @@ input[type="text" i] {
   font-size: 14px;
   height: 20px;
 }
+
+.task-lists-list {
+  display: flex;
+  margin: 12px 6px 0;
+  padding: 14px 8px;
+  border: 1px solid #B5B5B5;
+  border-radius: 12px;
+}
+
+.task-lists-list:first-child {
+  margin-top: 72px;
+}
+
+.task-lists-list label {
+  width: 100%;
+  display: flex;
+}
+
+.task-lists-list .to-memo {
+  width: 10px;
+  height: 10px;
+  margin: auto 4px;
+}
+.task-lists-list .trash {
+  width: 15px;
+  height: 15px;
+  margin: auto 0px;
+  opacity: 1;
+}
+
+.task-lists-list input[type="checkbox" i] {
+  margin: auto 0;
+  width: 20px;
+  height: 20px;
+  margin-right: 10px;
+}
+
+.task-lists-list p {
+  text-align: left;
+  font-size: 14px;
+  width: 90%;
+  max-width: 260px;
+}
+
+.checkbox-input {
+  display: none;
+}
+.checkbox-parts {
+  padding-left: 24px;
+  position: relative;
+}
+.checkbox-parts::before {
+  content: "";
+  display: block;
+  position: absolute;
+  top: 50%;
+  margin-top: -10px;
+  left: 0;
+  width: 15px;
+  height: 15px;
+  background-color: white;
+  border: 1px solid #fabe00;
+  border-radius: 50%;
+}
+.checkbox-input:checked + .checkbox-parts::after {
+  content: "";
+  display: block;
+  position: absolute;
+  top: 50%;
+  margin-top: -15px;
+  left: 5px;
+  width: 7px;
+  height: 14px;
+  transform: rotate(40deg);
+  border-bottom: 3px solid #fabe00;
+  border-right: 3px solid #fabe00;
+}
+
 
 </style>
