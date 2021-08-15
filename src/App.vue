daa10a3ea28f84e9d5a9090c99a6b97b5fabbb73
GitHub-Ryanghai <68055743+Ryanghai@users.noreply.github.com>
2021-08-15 22:18:55 +0900

    push for ask

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <PCSidebar/>
     <router-view/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import PCSidebar from "./views/PCSidebar.vue";
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  PCSidebar,
   render: h => h(App)
 }).$mount('#app')
